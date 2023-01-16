75ebe5b39e68953e062b101ad3073fe11e5dc381
thaxz <thais.monteiroc@ufpe.br>
2023-01-16 17:51:24 -0300

    first commit

---
+++ MapPlayground/MapPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPlaygroundApp.swift
+//  MapPlayground
+//
+//  Created by thaxz on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MapPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
