129e5d5ae458d7eb8b4c0d92604c303edf1e5a5c
thaxz <thais.monteiroc@ufpe.br>
2023-01-16 18:03:52 -0300

    feature(Assets): adding assets

---
+++ MapPlayground/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  MapPlayground
+//
+//  Created by thaxz on 16/01/23.
+//
+
+import Foundation
+import MapKit
+
+struct Location{
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
